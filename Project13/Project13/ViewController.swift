6b25ce194ac9afc250c4eba023324ef57f64e3ae
Jeff Eng <jeff@jeffeng.me>
2016-08-01 15:25:10 -0700

    Added Image Picker, and functionality for when user seleccts image or cancels Image Picker

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
+    
+    var currentImage: UIImage!
     
     @IBAction func changeFilter(sender: AnyObject) {
     }
@@ -24,7 +26,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Sets the title at the top
+        title = "Instafilter"
+        // Creates a '+' bar button item at the top and calls the importPicture method
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(importPicture))
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +39,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+    }
 
+    // This method is for selection of the picture from the Image Picker Controller
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var newImage: UIImage
+        
+        if let possibleImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            newImage = possibleImage
+        } else {
+            return
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        currentImage = newImage
+    }
+    
+    // This method closes the Image Picker Controller when user presses cancel
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
